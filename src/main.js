d9899a890ba3191dff65d1ce632c98d1dd4d107d
cheezenaan <cheezenaan@gmail.com>
2018-05-15 07:31:24 +0900

    Add eventlisteners

--- src/main.js
+++ src/main.js
@@ -29,11 +29,23 @@ new Vue({
       return `${friend.first} ${friend.last}`;
     },
   },
+  /* eslint-disable */
+  methods: {
+    incrementAge(friend) {
+      friend.age += 1;
+    },
+    decrementAge(friend) {
+      friend.age -= 1;
+    },
+  },
+  /* eslint-disable */
   template: `
     <div>
       <h2 v-for="friend in friends">
         <h4>{{friend | fullName}}</h4>
         <h5>age: {{friend | ageInOneYear}}</h5>
+        <button v-on:click="incrementAge(friend)">+</button>
+        <button v-on:click="decrementAge(friend)">-</button>
         <input v-model="friend.first"/>
         <input v-model="friend.last"/>
       </h2>
